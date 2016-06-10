b9caca08e5d18fc0e80abeb4e3ed50f5f9383891
Your Name <you@example.com>
2016-06-10 01:11:19 -0300

    Retweets consertados

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -3,7 +3,7 @@ class RetweetsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @retweet = Retweet.new(source_tweet_id: params[:tweet_id])
+    @retweet = Retweet.new(source_tweet_id: params[:tweet_id], retweeter: current_user)
     respond_to do |format|
       format.js
     end
